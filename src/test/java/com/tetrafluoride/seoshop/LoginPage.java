005754a4944068311e4b32ec9817ac39de8c635c
Anastasia Votchitseva <avvere@gmail.com>
2015-11-20 00:16:56 +0100

    New tests added

--- src/test/java/com/tetrafluoride/seoshop/LoginPage.java
+++ src/test/java/com/tetrafluoride/seoshop/LoginPage.java
@@ -5,6 +5,7 @@ package com.tetrafluoride.seoshop;
  */
 
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
@@ -14,7 +15,9 @@ import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private WebDriver driver;
@@ -32,71 +35,135 @@ public class LoginPage {
 
     @Test  //Normal login testing
     public void testLoginSuccess() throws Exception {
-        driver.get(baseUrl + "/login.html");
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
         driver.findElement(By.name("login")).clear();
         driver.findElement(By.name("login")).sendKeys("avvere@gmail.com");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("123456");
         driver.findElement(By.id("submitButton")).click();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.name("ShopAddress1"));
-        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("/firststart.php"));
+        driver.findElement(By.id("session_employee_name"));
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("?form_name=ui_tab&tab=choose_register"));
     }
 
 
     @Test  //Empty fields
     public void testLoginEmpty() throws Exception {
-        driver.get(baseUrl + "/login.html");
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
         driver.findElement(By.name("login")).clear();
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.id("submitButton")).click();
         driver.findElement(By.id("loginFailedMessage"));
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
     }
 
     @Test  //Wrong Password testing
     public void testLoginWrongPassword() throws Exception {
-        driver.get(baseUrl + "/login.html");
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
         driver.findElement(By.name("login")).clear();
         driver.findElement(By.name("login")).sendKeys("avvere@gmail.com");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("12345");
         driver.findElement(By.id("submitButton")).click();
         driver.findElement(By.id("loginFailedMessage"));
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
     }
 
     @Test  // password too long
     public void testLoginPasswordTooLong() throws Exception {
-        driver.get(baseUrl + "/login.html");
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
         driver.findElement(By.name("login")).clear();
         driver.findElement(By.name("login")).sendKeys("avvere@gmail.com");
         driver.findElement(By.name("password")).clear();
         Random r = new Random();
-        for (int i=0; i < 500; i++)
+        for (int i=0; i < 5; i++)
         {driver.findElement(By.name("password")).sendKeys(String.valueOf(r.nextInt(9)));}
         driver.findElement(By.id("submitButton")).click();
         driver.findElement(By.id("loginFailedMessage"));
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
     }
 
     @Test  //SQL injection testing
     public void testLoginSQL() throws Exception {
-        driver.get(baseUrl + "/login.html");
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
         driver.findElement(By.name("login")).clear();
         driver.findElement(By.name("login")).sendKeys("DROP TABLE user;");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("12345");
         driver.findElement(By.id("submitButton")).click();
         driver.findElement(By.id("loginFailedMessage"));
-        Assert.assertEquals(baseUrl + "/login.html", driver.getCurrentUrl());
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
     }
 
+    @Test  //Forgot password
+    public void testForgotPass() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.id("forgotPassword")).click();
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("/reset_password.html"));
+    }
+
+    @Test  //Status link
+    public void testStatus() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.linkText("status.lightspeedretail.com")).click();
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("status.lightspeedretail.com/"));
+    }
+
+    @Test  //Visit help
+    public void testVisitHelp() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.id("visitHelp")).click();
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("/support/retail/"));
+    }
+
+    @Test  //Register
+    public void testRegister() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.id("signUp")).click();
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("/register-for-a-free-trial/"));
+    }
+
+    @Test  //iPad Cloud link
+    public void testCloud() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.id("lsCloudForIpad")).click();
+
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("lightspeed-cloud-point-sale/id848366907"));
+    }
+
+    @Test  //Reporting link
+    public void testReport() throws Exception {
+        driver.get(baseUrl + "login.html");
+        Assert.assertEquals(baseUrl + "login.html", driver.getCurrentUrl());
+        driver.findElement(By.id("lsCloudReporting")).click();
+
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+
+        waitForLoad(driver);
+        Assert.assertTrue("Wrong redirect: " + driver.getCurrentUrl(), driver.getCurrentUrl().endsWith("retail/reporting/"));
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -107,6 +174,16 @@ public class LoginPage {
         }
     }
 
+    void waitForLoad(WebDriver driver) {
+        ExpectedCondition<Boolean> pageLoadCondition = new
+                ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver driver) {
+                        return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
+                    }
+                };
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(pageLoadCondition);
+    }
 
 }
 
